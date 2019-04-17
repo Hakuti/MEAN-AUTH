46c7e56c37c98ea871ac4c63af94ac57ce49bb19
Jean Canales <jeancanales@Jeans-MacBook-Pro.local>
2019-04-17 12:02:07 -0400

    ignore keys, connected mongoDB to express server.

--- lib/app.ts
+++ lib/app.ts
@@ -1,16 +1,22 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as mongoose from "mongoose";
+import { mongoURI } from "./config/keys";
 import { Routes } from "./routes/crmRoutes";
+
 
 class App {
 
     public app: express.Application;
     public routePrv: Routes = new Routes();
+    public mongoUrl: string = mongoURI;
+    
 
     constructor(){
         this.app = express();
         this.config();
         this.routePrv.routes(this.app);
+        this.mongoSetup();
     }
 
     private config(): void{
@@ -20,7 +26,11 @@ class App {
         //support application/x-www-form-url-encoded post data
         this.app.use(bodyParser.urlencoded({extended: false}));
 
+    }
 
+    private mongoSetup(): void {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);
     }
 }
 
---
+++ lib/config/keys.ts
@@ -0,0 +1,2 @@
+export const mongoURI = "mongodb+srv://Hakuti:Pwnage123@mern-auth-brzkw.mongodb.net/mean-test?retryWrites=true"
+    
